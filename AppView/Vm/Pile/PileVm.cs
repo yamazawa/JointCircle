fe766ee4fb12b528b891769a5a66f8e1cbad80ee
yamazawa <no.yamazawa@gmail.com>
2021-07-27 20:18:29 +0900

    [Pile実装]PileのViewModel実装

---
+++ AppView/Vm/Pile/PileVm.cs
@@ -0,0 +1,51 @@
+﻿using AppModel.IF.Pile;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+
+namespace AppView.Vm.Pile
+{
+    /// <summary>杭</summary>
+    public class PileVm : INotifyPropertyChanged
+    {
+        protected IPile Model;
+
+        public PileVm(IPile model)
+        {
+            Model = model;
+            Model.PropertyChanged += Model_PropertyChanged;
+        }
+
+        /// <summary>杭のID</summary>
+        public int Id
+        {
+            get => Model.Id;
+        }
+
+        /// <summary>座標</summary>
+        public Point Position
+        {
+            get => Model.Position;
+        }
+
+        /// <summary>Model側の値が変更された時の動作</summary>
+        protected virtual void Model_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            switch (e.PropertyName)
+            {
+                case nameof(Model.Position):
+                    RaisePropertyChanged(nameof(Position));
+                    break;
+            }
+        }
+
+        #region INotifyPropertyChanged実装
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
