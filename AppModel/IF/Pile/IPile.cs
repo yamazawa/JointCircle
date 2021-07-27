24e58f8c84a036e8da67817427a9e6d8e34fb868
yamazawa <no.yamazawa@gmail.com>
2021-07-27 20:00:30 +0900

    [Pile実装]Pile定義追加

---
+++ AppModel/IF/Pile/IPile.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Windows;
+
+namespace AppModel.IF.Pile
+{
+    /// <summary>杭</summary>
+    public interface IPile : INotifyPropertyChanged
+    {
+        /// <summary>杭のID</summary>
+        int Id { get; set; }
+
+        /// <summary>座標</summary>
+        Point Position { get; set; }
+    }
+}
---
+++ AppModel/Implement/Pile/Pile.cs
@@ -0,0 +1,44 @@
+﻿using AppModel.IF.Pile;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+
+namespace AppModel.Implement.Pile
+{
+    /// <summary>杭</summary>
+    internal class Pile : IPile
+    {
+        /// <summary>杭のID</summary>
+        public int Id
+        {
+            get => _id;
+            set
+            {
+                _id = value;
+                RaisePropertyChanged();
+            }
+        }
+        private int _id;
+
+        /// <summary>座標</summary>
+        public Point Position
+        {
+            get => _position;
+            set
+            {
+                _position = value;
+                RaisePropertyChanged();
+            }
+        }
+        private Point _position;
+
+        #region INotifyPropertyChanged実装
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
